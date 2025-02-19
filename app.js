d465084c4562a0ba7d5e422c9fd19679a870b2ea
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-02-19 15:00:10 +0530

    nothing

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
+app.get('/',(req,res)=>{
+    res.send("backend swiggy")
+})
 app.listen(PORT,()=>{
     console.log(`the server is running at ${PORT}`)
 })
